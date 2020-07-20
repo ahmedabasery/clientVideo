c6ba6a5704e8f4a8275c479149af1ba07eb4fa4b
ahmedabasery <engahmedabasery@yahoo.com>
2020-07-20 19:39:31 +0300

    Video stream player

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,11 +1,39 @@
 import React from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { FetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
-  componentDidMount() {
-    this.props.FetchStream(this.props.match.params.id);
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
   }
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.FetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return;
+    }
+    const { id } = this.props.match.params;
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
+  }
+
   render() {
     if (!this.props.stream) {
       return <div>Loading ...</div>;
@@ -13,9 +41,9 @@ class StreamShow extends React.Component {
     const { title, discription } = this.props.stream;
     return (
       <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
         <h1>{title}</h1>
         <h5>{discription}</h5>
-        <h6>Helo there</h6>
       </div>
     );
   }
